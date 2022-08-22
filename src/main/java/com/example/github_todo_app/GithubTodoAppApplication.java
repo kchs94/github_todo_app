215ed4353a10d47ca3e4f37c3c4c8dde781265d7
kchs94 <kchs94@gmail.com>
2022-08-22 16:55:22 +0900

    First Commit

---
+++ src/main/java/com/example/github_todo_app/GithubTodoAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.github_todo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTodoAppApplication.class, args);
+		System.out.println("hello worldㅋㅋㅋ");
+	}
+
+}
